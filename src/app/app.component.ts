7f171123fbd424243fb272ef36332b35280e0320
MaitenCullen <cullen.maiten@gmail.com>
2023-10-17 12:49:59 -0300

    nuevas rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
@@ -8,13 +9,19 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 })
 export class AppComponent {
   
-  constructor(private modalService: NgbModal) {
+  constructor(private modalService: NgbModal, private router: Router) {
 
   }
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0); // Esto desplaza la página al inicio.
+      }
+    });
+  }
   public open(modal: any): void {
-
     this.modalService.open(modal);
-
   }
   title = '9-julio';
 }
